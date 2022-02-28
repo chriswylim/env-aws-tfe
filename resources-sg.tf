132747c493b72c590d066842d2c32aa42100751a
StuckInOffice <limwei.yew@petronas.com>
2022-02-28 15:33:33 +0800

    ! initialize

--- resources-sg.tf
+++ resources-sg.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["10.0.0.0/8"]
   }
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/8"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
